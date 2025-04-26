b17db600aedfccba5d85152fdace47aafe9ae8c1
antonioscapellato <antonio.scape99@gmail.com>
2025-04-26 17:46:07 +0200

    less stuff

--- agent_tool/event_test.py
+++ agent_tool/event_test.py
@@ -10,8 +10,8 @@ async def main(agent_id: str, run_id: str) -> None:
     await client.send_agent_event(
         agent_id=agent_id,
         run_id=run_id,
-        event_name="messages",
-        event_input={"messages": [{"role": "user", "content": "Tell me another joke"}]},
+        event_name="message",
+        event_input={"content": "Tell me another joke"},
     )
 
     await client.send_agent_event(
@@ -23,9 +23,14 @@ async def main(agent_id: str, run_id: str) -> None:
     sys.exit(0)
 
 
-def run_event_workflow() -> None:
-    asyncio.run(main(agent_id="agent-id", run_id="run-id"))
+def run_event_agent() -> None:
+    asyncio.run(
+        main(
+            agent_id="1739788461173-AgentChat",
+            run_id="c3937cc9-8d88-4e37-85e1-59e78cf1bf60",
+        )
+    )
 
 
 if __name__ == "__main__":
-    run_event_workflow()
+    run_event_agent()
